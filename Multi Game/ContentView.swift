c17bbfbae1690eb54b0f99b5aa7788b56fb0056a
Petter <petter.gustafsson@hotmail.com>
2023-10-03 07:50:53 +0200

    Initial Commit

---
+++ Multi Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Multi Game
+//
+//  Created by Petter Gustafsson on 2023-10-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Multi Game/Multi_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multi_GameApp.swift
+//  Multi Game
+//
+//  Created by Petter Gustafsson on 2023-10-03.
+//
+
+import SwiftUI
+
+@main
+struct Multi_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
